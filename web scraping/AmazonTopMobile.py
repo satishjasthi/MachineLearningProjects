900630655c306dae0202f10e771cd06617c67756
mohansaimandalapu <mohansaimandalapu@gmail.com>
2021-01-20 12:17:58 +0530

    Adding Web scraping of Amazon data

--- web scraping/AmazonTopMobile.py
+++ web scraping/AmazonTopMobile.py
@@ -7,20 +7,29 @@ import schedule
 import time
 from csv import writer 
 
+import schedule
+import time
+
+
+# function to get the top 10 mobile list and there discriptions
 def Amazon_Top_mobiles():
 
+
+    # Adding url of amazon on crome driver
     driver = webdriver.Chrome(ChromeDriverManager().install())
     url = 'https://www.amazon.in/s?k=top+10+mobiles&ref=nb_sb_noss_1'
     driver.get(url)
-
+    
+    #creating soup object of the crome page to extract
     soup = BeautifulSoup(driver.page_source,'lxml')
     result = soup.find_all('div',{'data-component-type':'s-search-result'})
 
+    #function to get Mobile name , specifications , Price , Ratings
     data =[]
     for i  in range(0,10):
         item = result[i]
         atag = item.h2.a
-        # extracting Mobile name
+        # extracting Mobile name and Description(RAM and storage)
         mobile =  atag.text.strip()
         ## extracting price
         try:
@@ -36,11 +45,14 @@ def Amazon_Top_mobiles():
             rating = None
             Number_of_rating = None
         
+        #appending the discrption into data list
         discription = (mobile,price,rating,Number_of_rating)
         data.append(list(discription))
 
     print(data)
+    
 
+    # converting data list into csv file
     for i in range(len(data)):
         # Open our existing CSV file in append mode 
         # Create a file object for this file 
@@ -58,9 +70,7 @@ def Amazon_Top_mobiles():
             f_object.close() 
 
 
-import schedule
-import time
-
+#scheduling to run program for every 24 hours
 schedule.every(24).hours.do(Amazon_Top_mobiles)
 
 while True:
